2c5fa8df36592e4706db11510f77e17590036aab
spykelion <ndilionel6@gmail.com>
2022-09-20 14:41:17 +0100

    update hbs call

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/' }));
+app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/' }));
 app.set('view engine', 'hbs');
 
 //Home route
